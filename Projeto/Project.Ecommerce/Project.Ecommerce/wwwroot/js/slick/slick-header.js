a5e250a47c58174421ac1c4bcb0db89f26b71019
Felipe Luz <fh127@CTS1C100056>
2022-03-25 15:23:23 -0300

    Feat

--- Projeto/Project.Ecommerce/Project.Ecommerce/wwwroot/js/slick/slick-header.js
+++ Projeto/Project.Ecommerce/Project.Ecommerce/wwwroot/js/slick/slick-header.js
@@ -6,3 +6,13 @@
         speed: 200,
         autoplay: true
     });
+
+
+$("#modal").iziModal();
+
+$('#teste').on('click', '.trigger', function (event) {
+    event.preventDefault();
+    // $('#modal').iziModal('setZindex', 99999);
+    // $('#modal').iziModal('open', { zindex: 99999 });
+    $('#modal').iziModal('open');
+});
