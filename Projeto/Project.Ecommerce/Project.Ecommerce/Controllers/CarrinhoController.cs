b803255390ad3ae085690e966ae5fe0dac6979da
Felipe Luz <fh127@CTS1C100056>
2022-03-26 09:37:57 -0300

    Feat - Criação de chamadas HTML

---
+++ Projeto/Project.Ecommerce/Project.Ecommerce/Controllers/CarrinhoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Ecommerce.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projeto/Project.Ecommerce/Project.Ecommerce/Controllers/PerfilController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Ecommerce.Controllers
+{
+    public class PerfilController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
