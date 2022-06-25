2659dd927dadb64945d48867ed1f11b79f746087
Felipe Luz <fh127@CTS1C100056>
2022-06-25 12:00:31 -0300

    Feat - DB

--- Projeto/Project.Ecommerce/src/Project.Ecommerce/Controllers/UsuarioController.cs
+++ Projeto/Project.Ecommerce/src/Project.Ecommerce/Controllers/UsuarioController.cs
@@ -5,17 +5,17 @@ namespace Project.Ecommerce.Controllers
 {
     public class UsuarioController : Controller
     {
-        //private readonly IUsuarioAppService _usuarioAppService;
+        private readonly IUsuarioAppService _usuarioAppService;
 
-        //public UsuarioController(IUsuarioAppService usuarioAppService)
-        //{
-        //    _usuarioAppService = usuarioAppService;
-        //}
+        public UsuarioController(IUsuarioAppService usuarioAppService)
+        {
+            _usuarioAppService = usuarioAppService;
+        }
 
         [Route("/BuscarUsuario")]
         public IActionResult Listar()
         {
-            //var usuarios = _usuarioAppService.Listar();
+            var usuarios = _usuarioAppService.Listar();
             return View();
         }
     }
